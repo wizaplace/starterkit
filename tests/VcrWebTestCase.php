06f066b5c47a2bc8d768ccdc7df0a54aa040501a
Hector Jusforgues <hector.jusforgues@gmail.com>
2017-06-06 13:16:37 +0200

    Simple Prefixed Slug Handling (#13)
    * Simple Prefixed Slug Handling
    
    * coke
    
    * Try fixing tests
    
    * Try fixing tests
    
    * Try fixing tests
    
    * Fix tests

---
+++ tests/VcrWebTestCase.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @author      Wizacha DevTeam <dev@wizacha.com>
+ * @copyright   Copyright (c) Wizacha
+ * @license     Proprietary
+ */
+declare(strict_types = 1);
+
+namespace Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use VCR\VCR;
+
+abstract class VcrWebTestCase extends WebTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        VCR::turnOn();
+        $cassette = (new \ReflectionClass($this))->getShortName().DIRECTORY_SEPARATOR.$this->getName().'.yml';
+        VCR::insertCassette($cassette);
+    }
+
+    protected function tearDown(): void
+    {
+        VCR::turnOff();
+
+        parent::tearDown();
+    }
+}
