02c7844a9e2b42c81d2b6bc7e9c81bdfc246796b
hectorj <hector.jusforgues@gmail.com>
2017-06-09 17:05:26 +0200

    Remove a bundle we don't need

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,7 +21,6 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-            $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
         }
 
         return $bundles;
