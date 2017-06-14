e58fa159d3d957ef60f4bd440c57a706147cb8d6
hectorj <hector.jusforgues@gmail.com>
2017-06-14 10:56:04 +0200

    Functional tests: disable opcache for Behat

--- tests/behat/docker/entrypoint.sh
+++ tests/behat/docker/entrypoint.sh
@@ -4,8 +4,8 @@ set -x
 
 /usr/bin/google-chrome --disable-gpu --headless --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 --no-sandbox &
 
-bin/console server:start 127.0.0.1:8080
+bin/console --env=test server:start 127.0.0.1:8080
 
 export TEST_WEBSERVER_URL=http://127.0.0.1/app_test.php/
 
-vendor/bin/behat --config behat.yml --format=pretty --out=std --format=junit --out=behat-result
+php -d opcache.enable=0 vendor/bin/behat --config behat.yml --format=pretty --out=std --format=junit --out=behat-result
