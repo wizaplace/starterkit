6fae7b75be62f0adca1169250b9ef5bd769a0afc
hectorj <hector.jusforgues@gmail.com>
2017-07-03 17:27:56 +0200

    Add a simple login test

--- tests/behat/contexts/GenericPageContext.php
+++ tests/behat/contexts/GenericPageContext.php
@@ -9,9 +9,41 @@ declare(strict_types = 1);
 namespace Tests\behat\contexts;
 
 use Assert\Assertion;
+use Behat\Mink\Exception\DriverException;
 
 class GenericPageContext extends AbstractPageContext
 {
+    /**
+     * @Given I am not logged in
+     */
+    public function givenIAmNotLoggedIn()
+    {
+        try {
+            $this->getSession()->setCookie('PHPSESSID', null);
+        } catch (DriverException $e) {
+            // If we get an exception because no page was visited, it means we were not logged in.
+            if ($e->getMessage() !== 'Unable to access the request before visiting a page') {
+                throw $e;
+            }
+        }
+    }
+
+    /**
+     * @Given /^I am logged in as "(?P<user>[^"]+)" with password "(?P<password>[^"]+)"$/
+     */
+    public function givenIAmLoggedIn(string $user, string $password)
+    {
+        try {
+            $this->visitPath("/login");
+            $this->waitForFind('input[name=email]')->setValue($user);
+            $this->waitForFind('input[name=password]')->setValue($password);
+            $this->waitForFind('button[type=submit].validate')->click();
+            $this->assertSession()->statusCodeEquals(200);
+        } catch (\Exception $e) {
+            throw new \Exception("Failed to login", $e->getCode(), $e);
+        }
+    }
+
     /**
      * @Then /^the page meta title should be "(?P<expectedTitle>[^"]+)"$/
      */
