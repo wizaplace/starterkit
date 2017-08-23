3cc036e095b8a2a4e669fea77450769601914e82
Serge Mazille <serge@wizacha.com>
2017-08-23 16:08:11 +0200

    Added discussion feature

--- src/AppBundle/Controller/ProfileController.php
+++ src/AppBundle/Controller/ProfileController.php
@@ -4,140 +4,40 @@
  * @copyright   Copyright (c) Wizacha
  * @license     Proprietary
  */
+declare(strict_types = 1);
 
 namespace AppBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Translation\TranslatorInterface;
-use Wizaplace\ApiClient;
-use Wizaplace\Authentication\BadCredentials;
-use Wizaplace\Order\OrderService;
-use Wizaplace\User\User as WizaplaceUser;
-use Wizaplace\User\UserService;
 use WizaplaceFrontBundle\Security\User;
+use WizaplaceFrontBundle\Controller\ProfileController as BaseController;
 
-class ProfileController extends Controller
+class ProfileController extends BaseController
 {
-    private const PASSWORD_MINIMUM_LENGTH = 6;
-
-    /** @var TranslatorInterface */
-    private $translator;
-
-    public function __construct(TranslatorInterface $translator)
-    {
-        $this->translator = $translator;
-    }
-
     public function viewAction(): Response
     {
-        return $this->render('@App/profile/profile.html.twig', [
-            'profile' => $this->getUser()->getWizaplaceUser(),
-        ]);
+        return parent::viewAction();
     }
 
     public function addressesAction(): Response
     {
-        return $this->render('@App/profile/addresses.html.twig', [
-            'profile' => $this->getUser()->getWizaplaceUser(),
-        ]);
+        return parent::addressesAction();
     }
 
     public function ordersAction(): Response
     {
-        $orders = $this->get(OrderService::class)->getOrders();
-
-        return $this->render('@App/profile/orders.html.twig', [
-            'profile' => $this->getUser()->getWizaplaceUser(),
-            'orders' => $orders,
-        ]);
+        return parent::ordersAction();
     }
 
     public function returnsAction(): Response
     {
-        return $this->render('@App/profile/returns.html.twig', [
-            'profile' => $this->getUser()->getWizaplaceUser(),
-        ]);
+        return parent::returnsAction();
     }
 
     public function updateProfileAction(Request $request)
     {
-        $data = $request->request->get('user');
-        $sameAddress = $request->request->get('sameAddress');
-        $referer = $request->headers->get('referer');
-        $submittedToken = $request->get('csrf_token');
-
-        $user = new WizaplaceUser($data);
-        $userService = $this->get(UserService::class);
-
-        // CSRF token validation
-        if (! $this->isCsrfTokenValid('profile_update_token', $submittedToken)) {
-            $message = $this->translator->trans('csrf_error_message');
-            $this->addFlash('warning', $message);
-
-            return $this->redirect($referer);
-        }
-
-        // update user's password
-        if (! empty($data['password'])) {
-            $newPassword = $data['password']['new'];
-
-            // check new password corresponds to password rules
-            if (strlen($newPassword) < self::PASSWORD_MINIMUM_LENGTH) {
-                $message = $this->translator->trans('update_new_password_error_message', ['%n%' => self::PASSWORD_MINIMUM_LENGTH]);
-                $this->addFlash('danger', $message);
-
-                return $this->redirect($referer);
-            }
-
-            // check user's old credentials
-            $oldPassword = $data['password']['old'];
-            $api = $this->get(ApiClient::class);
-
-            try {
-                $api->authenticate($user->getEmail(), $oldPassword);
-            } catch (BadCredentials $e) {
-                $message = $this->translator->trans('update_old_password_error_message');
-                $this->addFlash('danger', $message);
-
-                return $this->redirect($referer);
-            }
-
-            $userService->changePassword($user->getId(), $newPassword);
-
-            // add a notification
-            $message = $this->translator->trans('update_password_success_message');
-            $this->addFlash('success', $message);
-        }
-
-        // update user's profile
-        $userService->updateUser($user->getId(), $user->getEmail(), $user->getFirstname(), $user->getLastname());
-
-        $message = $this->translator->trans('update_profile_success_message');
-        $this->addFlash('success', $message);
-
-        // Override shipping address fields with billing ones if both are the same (else do nothing)
-        if ($sameAddress) {
-            // actual override
-            $data['addresses']['shipping'] = $data['addresses']['billing'];
-
-            // Petite manip pour les champs de profil qui ont un Id qui est different dans billing et shipping
-            $data['addresses']['shipping'][38] = $data['addresses']['shipping'][37];
-            unset($data['addresses']['shipping'][37]);
-            $data['addresses']['shipping'][40] = $data['addresses']['shipping'][39];
-            unset($data['addresses']['shipping'][39]);
-        }
-
-        // update user's addresses
-        if (! empty($data['addresses'])) {
-            $userService->updateUserAdresses($user);
-
-            $message = $this->translator->trans('update_addresses_success_message');
-            $this->addFlash('success', $message);
-        }
-
-        return $this->redirect($referer);
+        return parent::updateProfileAction($request);
     }
 
     // This method sole purpose is the return type hint.
---
+++ src/WizaplaceFrontBundle/Controller/DiscussionController.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * @author      Wizacha DevTeam <dev@wizacha.com>
+ * @copyright   Copyright (c) Wizacha
+ * @license     Proprietary
+ */
+declare(strict_types = 1);
+
+namespace WizaplaceFrontBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Translation\TranslatorInterface;
+use Wizaplace\Discussion\DiscussionService;
+
+class DiscussionController extends Controller
+{
+    /** @var TranslatorInterface */
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    public function createAction(Request $request): Response
+    {
+        // CSRF token validation
+        $referer = $request->headers->get('referer');
+        $submittedToken = $request->request->get('csrf_token');
+
+        if (! $this->isCsrfTokenValid('discussion_token', $submittedToken)) {
+            $notification = $this->translator->trans('csrf_error_message');
+            $this->addFlash('warning', $notification);
+
+            return $this->redirect($referer);
+        }
+
+        // create a new discussion and post message
+        $productId = (int) $request->request->get('product_id');
+        $discussionMessage = $request->get('discussion_message');
+        $discussionService = $this->get(DiscussionService::class);
+        $discussionData = $discussionService->startDiscussion($productId);
+
+        try {
+            $discussionService->postMessage($discussionData->getId(), $discussionMessage);
+        } catch (\Exception $exception) {
+            // add alert notification
+            $notification = $this->translator->trans('message_sent_error_notification');
+            $this->addFlash('danger', $notification);
+
+            return $this->redirect($referer);
+        }
+
+        // add success notification
+        $notification = $this->translator->trans('message_sent_success_notification');
+        $this->addFlash('success', $notification);
+
+        // redirect user to product page
+        return $this->redirect($referer);
+    }
+
+    public function createMessageAction(Request $request): Response
+    {
+        // CSRF token validation
+        $referer = $request->headers->get('referer');
+        $submittedToken = $request->request->get('csrf_token');
+
+        if (! $this->isCsrfTokenValid('discussion_token', $submittedToken)) {
+            $notification = $this->translator->trans('csrf_error_message');
+            $this->addFlash('warning', $notification);
+
+            return $this->redirect($referer);
+        }
+
+        // add a message to the discussion
+        $discussionId = (int) $request->get('discussion_id');
+        $discussionMessage = $request->get('discussion_message');
+        $discussionService = $this->get(DiscussionService::class);
+
+        try {
+            $discussionService->postMessage($discussionId, $discussionMessage);
+        } catch (\Exception $exception) {
+            // add alert notification
+            $notification = $this->translator->trans('message_sent_error_notification');
+            $this->addFlash('danger', $notification);
+
+            return $this->redirect($referer);
+        }
+
+        $discussion = $discussionService->getDiscussion($discussionId);
+        $messages = $discussionService->getMessages($discussionId);
+
+        // add success notification
+        $notification = $this->translator->trans('message_sent_success_notification');
+        $this->addFlash('success', $notification);
+
+        return $this->redirectToRoute('profile_message', [
+            'discussion_id' => $discussionId,
+            'discussion' => $discussion,
+            'messages' => $messages,
+            'profile' => $this->getUser()->getWizaplaceUser(),
+        ]);
+    }
+}
