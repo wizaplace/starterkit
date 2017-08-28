dab6b7c8196099cd45c06ba31777fc8ba23aba8b
hectorj <hector.jusforgues@gmail.com>
2017-08-28 16:31:37 +0200

    Remove Behat tests entirely

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -11,16 +11,4 @@ sudo cp /vagrant/vagrant/apache.conf /etc/apache2/sites-available/apache.conf
 sudo a2ensite apache.conf
 sudo service apache2 restart
 
-# Chrome Headless Setup
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb || \
-sudo apt-get install -y --fix-broken
-
-# Configuration spécifique de supervisor
-sudo cp /vagrant/vagrant/supervisor.conf /etc/supervisor/conf.d/wizaplace.conf
-sudo mkdir -p /var/log/chrome-headless/
-sudo service supervisor restart
-
-echo "export TEST_WEBSERVER_URL=http://127.0.0.1/app_test.php/" >> /home/vagrant/.zprofile
-
 cd /vagrant && make install
