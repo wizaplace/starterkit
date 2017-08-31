d637eeb4b8f5e4d0ac3c348add016aee802bbdfb
hectorj <hector.jusforgues@gmail.com>
2017-08-31 14:54:03 +0200

    Document AuthController's constants

--- src/WizaplaceFrontBundle/Controller/AuthController.php
+++ src/WizaplaceFrontBundle/Controller/AuthController.php
@@ -14,12 +14,35 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class AuthController extends Controller
 {
+    /**
+     * @var string field name to be used for the email at login
+     */
     public const EMAIL_FIELD_NAME = 'email';
+
+    /**
+     * @var string field name to be used for the password at login
+     */
     public const PASSWORD_FIELD_NAME = 'password';
+
+    /**
+     * @var string field name to be used for the url we wish to be redirected to after login/logout
+     */
     public const REDIRECT_URL_FIELD_NAME = 'redirect_url';
+
+    /**
+     * @var string field name to be used for the CSRF token at login/logout
+     */
     public const CSRF_FIELD_NAME = 'csrf_token';
+
+    /**
+     * @var string CSRF token to be used for login
+     */
     public const CSRF_LOGIN_ID = 'login_token';
-    public const CSRF_LOGOUT_ID = 'login_token';
+
+    /**
+     * @var string CSRF token to be used for logout
+     */
+    public const CSRF_LOGOUT_ID = 'logout_token';
 
     public function loginAction(Request $request): Response
     {
