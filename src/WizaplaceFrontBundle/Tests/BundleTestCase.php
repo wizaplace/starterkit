b76f1238581ab81b2d1264c48a3f84a1863f582c
Hector Jusforgues <hector.jusforgues@gmail.com>
2017-09-04 14:56:47 +0200

    fix bad rebase

--- src/WizaplaceFrontBundle/Tests/BundleTestCase.php
+++ src/WizaplaceFrontBundle/Tests/BundleTestCase.php
@@ -52,7 +52,6 @@ abstract class BundleTestCase extends WebTestCase
     protected function tearDown()
     {
         self::$cassetteName = null;
-        $this->client = null;
         self::$kernel->getContainer()->get(VcrGuzzleMiddleware::class)->getVcr()->turnOff();
         $this->client = null;
         parent::tearDown();
