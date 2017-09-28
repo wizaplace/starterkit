9d75adff2ae2a5035333e296c2a3e08a6e0182f8
hectorj <hector.jusforgues@gmail.com>
2017-09-28 16:29:57 +0200

    Introduce a BasketService

---
+++ src/WizaplaceFrontBundle/Service/BasketService.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * @copyright Copyright (c) Wizacha
+ * @license Proprietary
+ */
+declare(strict_types = 1);
+
+namespace WizaplaceFrontBundle\Service;
+
+use GuzzleHttp\Exception\ClientException;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Wizaplace\SDK\Basket\Basket;
+use Wizaplace\SDK\Basket\PaymentInformation;
+
+class BasketService
+{
+    private const ID_SESSION_KEY = '_basketId';
+
+    /** @var  \Wizaplace\SDK\Basket\BasketService */
+    private $baseService;
+
+    /** @var SessionInterface */
+    private $session;
+
+    /** @var Basket */
+    private $basket;
+
+    public function __construct(\Wizaplace\SDK\Basket\BasketService $baseService, SessionInterface $session)
+    {
+        $this->baseService = $baseService;
+        $this->session = $session;
+    }
+
+    public function getBasket(): Basket
+    {
+        $basketId = $this->getBasketId();
+        if (!$this->basket) {
+            try {
+                $this->basket = $this->baseService->getBasket($basketId);
+            } catch (ClientException $e) {
+                if ($e->getResponse()->getStatusCode() === 404) {
+                    $this->forgetBasketId();
+
+                    return $this->getBasket();
+                }
+            }
+        }
+
+        return $this->basket;
+    }
+
+    public function addProductToBasket(string $declinationId, int $quantity): int
+    {
+        return $this->baseService->addProductToBasket($this->getBasketId(), $declinationId, $quantity);
+    }
+
+    public function removeProductFromBasket(string $declinationId): void
+    {
+        $this->baseService->removeProductFromBasket($this->getBasketId(), $declinationId);
+    }
+
+    public function cleanBasket(): void
+    {
+        $this->baseService->cleanBasket($this->getBasketId());
+    }
+
+    public function updateProductQuantity(string $declinationId, int $quantity): int
+    {
+        return $this->baseService->updateProductQuantity($this->getBasketId(), $declinationId, $quantity);
+    }
+
+    public function addCoupon(string $coupon): void
+    {
+        $this->baseService->addCoupon($this->getBasketId(), $coupon);
+    }
+
+    public function removeCoupon(string $coupon): void
+    {
+        $this->baseService->removeCoupon($this->getBasketId(), $coupon);
+    }
+
+    public function getPayments(): array
+    {
+        return $this->baseService->getPayments($this->getBasketId());
+    }
+
+    public function selectShippings(array $selections): void
+    {
+        $this->baseService->selectShippings($this->getBasketId(), $selections);
+    }
+
+    public function checkout(int $paymentId, bool $acceptTerms, string $redirectUrl): PaymentInformation
+    {
+        return $this->baseService->checkout($this->getBasketId(), $paymentId, $acceptTerms, $redirectUrl);
+    }
+
+    /**
+     * Gets current basket ID, or create a new one
+     * @return string
+     */
+    private function getBasketId(): string
+    {
+        $basketId = $this->session->get(self::ID_SESSION_KEY);
+
+        if (null === $basketId) {
+            $basketId = $this->baseService->create();
+            $this->session->set(self::ID_SESSION_KEY, $basketId);
+        }
+
+        return $basketId;
+    }
+
+    private function forgetBasketId(): void
+    {
+        $this->session->remove(self::ID_SESSION_KEY);
+    }
+}
