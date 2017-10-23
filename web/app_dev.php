e19db95e34f73f3a2ac402b05fb1c70a4958c7fd
hectorj <hector.jusforgues@gmail.com>
2017-10-23 10:06:54 +0200

    Deny access to app_dev.php only on master branch

--- web/app_dev.php
+++ web/app_dev.php
@@ -1,7 +1,12 @@
 <?php
 
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
+use Symfony\Component\HttpFoundation\Request;
+
+if (getenv('PLATFORM_BRANCH') === 'master') {
+    header('HTTP/1.0 403 Forbidden');
+    exit;
+}
 
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/setup.html#checking-symfony-application-configuration-and-setup
