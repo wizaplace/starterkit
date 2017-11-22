e06b22e4a5873a831e5137dedc3f05ba1d54f4a8
Serge Mazille <serge@wizaplace.com>
2017-11-22 17:20:49 +0100

    Home banners

--- src/AppBundle/Resources/public/scripts/includes/ui.js
+++ src/AppBundle/Resources/public/scripts/includes/ui.js
@@ -91,20 +91,6 @@ const ui = {
         });
     },
 
-    // slick
-    initSlick: function() {
-        // home page banners
-        $('.banners').find('[class*="-screens"]').slick({
-            dots: true,
-            infinite: true,
-            autoplay: true,
-            autoplaySpeed: 6000,
-            speed: 300, // transition speed
-            slidesToShow: 1,
-            slidesToScroll: 1,
-        });
-    },
-
     // slick legacy
     initSlickShowcase: function() {
         let $arrows = $('.arrows');
