c9a5536ea34c2668f74e5bec92bc49deb685161e
Nicolas Gomes De Oliveira <ngdo27@gmail.com>
2018-01-11 09:17:24 +0100

    remove blankline, add translation

--- src/AppBundle/Controller/ContactController.php
+++ src/AppBundle/Controller/ContactController.php
@@ -1,11 +1,9 @@
 <?php
 /**
- * @author      Wizacha DevTeam <dev@wizacha.com>
  * @copyright   Copyright (c) Wizacha
  * @license     Proprietary
  */
 declare(strict_types=1);
-
 
 namespace AppBundle\Controller;
 
