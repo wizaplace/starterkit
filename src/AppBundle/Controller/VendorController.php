8df6494343c1bd88573d07319406263a96a68432
Nicolas Gomes De Oliveira <ngdo27@gmail.com>
2018-03-28 17:14:10 +0200

    add createProduct's form (form not handled yet)

--- src/AppBundle/Controller/VendorController.php
+++ src/AppBundle/Controller/VendorController.php
@@ -8,10 +8,17 @@ declare(strict_types=1);
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Translation\TranslatorInterface;
+use Wizaplace\SDK\Catalog\CatalogServiceInterface;
+use Wizaplace\SDK\Catalog\Category;
+use Wizaplace\SDK\Pim\Product\CreateProductCommand;
+use Wizaplace\SDK\Pim\Product\ProductGeolocationUpsertData;
+use Wizaplace\SDK\Pim\Product\ProductService;
+use Wizaplace\SDK\Pim\Product\ProductStatus;
 
 class VendorController extends Controller
 {
@@ -20,9 +27,24 @@ class VendorController extends Controller
      */
     private $translator;
 
-    public function __construct(TranslatorInterface $translator)
-    {
+    /**
+     * @var ProductService
+     */
+    private $productService;
+
+    /**
+     * @var CatalogServiceInterface
+     */
+    private $catalogService;
+
+    public function __construct(
+        TranslatorInterface $translator,
+        ProductService $productService,
+        CatalogServiceInterface $catalogService
+    ) {
         $this->translator = $translator;
+        $this->productService = $productService;
+        $this->catalogService = $catalogService;
     }
 
     public function dashboardSummaryAction(): Response
@@ -32,6 +54,43 @@ class VendorController extends Controller
         }
 
         return $this->render('@App/vendor/dashboard-summary.html.twig');
+    }
+
+    public function createProductAction(): Response
+    {
+        if (!$this->isAccessGranted()) {
+            return $this->redirectToRoute('home');
+        }
+
+        $statusList = ProductStatus::toArray();
+        $categoriesList = $this->catalogService->getCategories();
+
+        $productStatusList = [];
+        foreach ($statusList as $status) {
+            $productStatusList[] = [
+                'value' => $status,
+                'name'  => $this->translator->trans('vendor.products.creation.status.'.$status),
+            ];
+        }
+        $categoriesList = array_map(static function ($category){
+            /** @var Category $category */
+            return [
+                'value' => $category->getId(),
+                'name'  => $category->getName(),
+            ];
+        }, $categoriesList);
+        $taxList = [ //TODO: récupérer les vraies taxes de l'API via le SDK
+            [
+                'value' => 0,
+                'name' => 'TVA 20%',
+            ]
+        ];
+
+        return $this->render('@App/vendor/products/create.html.twig', [
+            'productStatusList' => $productStatusList,
+            'categoriesList'    => $categoriesList,
+            'taxList'           => $taxList,
+        ]);
     }
 
     /**
