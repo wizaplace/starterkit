a4e2bf00fd902bced1a7d1340fdd1f8ec05f14c2
Florent Viel <luxifer666@gmail.com>
2018-03-29 09:49:29 +0200

    configure trusted proxies to get real IP

--- web/app_dev.php
+++ web/app_dev.php
@@ -20,6 +20,15 @@ Debug::enable();
 
 $kernel = new AppKernel('dev', true);
 $request = Request::createFromGlobals();
+
+// Uniquement quand on est dans le cluster k8s
+if (false !== getenv('KUBERNETES_SERVICE_HOST')) {
+    // On fait confiance à toutes les requêtes qui arrivent,  dans le cas ou on est
+    // en direct, REMOTE_ADDR correspond à l'adresse réelle du client, dans le cas
+    // de k8s, elle correspond à l'adresse de l'ingress
+    Request::setTrustedProxies(['127.0.0.1', $request->server->get('REMOTE_ADDR')], Request::HEADER_X_FORWARDED_ALL);
+}
+
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
