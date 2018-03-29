d709394739a7b84b3a058f7204e7e83f85df4b8e
Hector Jusforgues <hector.jusforgues@gmail.com>
2018-03-29 10:19:52 +0200

    Proxy AJAX search calls (#276)
    * Proxy AJAX search calls
    
    * remove console.log
    
    * Proxied search: add geofilter

--- src/AppBundle/Controller/SearchController.php
+++ src/AppBundle/Controller/SearchController.php
@@ -8,18 +8,36 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Wizaplace\SDK\Catalog\CatalogService;
+use Wizaplace\SDK\Catalog\CatalogServiceInterface;
+use Wizaplace\SDK\Catalog\GeoFilter;
 use WizaplaceFrontBundle\Service\FavoriteService;
+use WizaplaceFrontBundle\Service\JsonSearchService;
 
 class SearchController extends Controller
 {
+    /**
+     * @var CatalogServiceInterface
+     */
+    private $catalogService;
+
+    /**
+     * @var JsonSearchService
+     */
+    private $jsonSearchService;
+
+    public function __construct(CatalogServiceInterface $catalogService, JsonSearchService $jsonSearchService)
+    {
+        $this->catalogService = $catalogService;
+        $this->jsonSearchService = $jsonSearchService;
+    }
+
     public function searchAction(Request $request): Response
     {
-        $catalogService = $this->get(CatalogService::class);
         $selectedCategoryId = $request->query->getInt("selected_category_id");
-        $selectedCategory = $selectedCategoryId ? $catalogService->getCategory($selectedCategoryId) : null;
+        $selectedCategory = $selectedCategoryId ? $this->catalogService->getCategory($selectedCategoryId) : null;
 
         $filters = [];
         if ($selectedCategoryId) {
@@ -34,5 +52,21 @@ class SearchController extends Controller
             'selectedCategory' => $selectedCategory,
             'userFavoriteIds' => $userFavoriteIds,
         ]);
+    }
+
+    public function jsonSearchAction(Request $request): JsonResponse
+    {
+        $query = $request->query->get('query', '');
+        $filters = $request->query->get('filters', []);
+        $sorting = $request->query->get('sorting', []);
+        $resultsPerPage = $request->query->getInt('resultsPerPage', 12);
+        $page = $request->query->getInt('page', 1);
+        $geoFilter = null;
+        if ($request->query->has('geo')) {
+            $geo = $request->query->get('geo');
+            $geoFilter = new GeoFilter($geo['lat'], $geo['lng'], $geo['radius'] ?? null);
+        }
+
+        return new JsonResponse($this->jsonSearchService->search($query, $filters, $sorting, $resultsPerPage, $page, $geoFilter), Response::HTTP_OK, [], true);
     }
 }
