07f1781e385369cf61720f524ef449e1ad719a98
Philippe VANDERMOERE <philippe@wizacha.com>
2018-09-20 15:42:39 +0200

    check if prod.json exist before gzip

--- bin/logrotate.sh
+++ bin/logrotate.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-cd /app/var/logs && gzip -c prod.json > prod.json.$(date "+%Y-%m-%d_%H-%M-%S").gz && echo -n > prod.json && find . -type f -name "*.gz" -mtime +7 -delete
+set -e
+
+cd /app/var/logs
+
+if [ -f prod.json ]; then
+    gzip -c prod.json > prod.json.$(date "+%Y-%m-%d_%H-%M-%S").gz
+    echo -n > prod.json
+fi
+
+find . -type f -name "*.gz" -mtime +7 -delete
